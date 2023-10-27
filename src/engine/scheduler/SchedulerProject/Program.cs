2d01374719cbecf94d1c5d2d24e87acf3570a056
Olivier Vermeir <olivier.vermeir@ignyte.be>
2023-10-27 17:57:32 +0200

    Prototype scheduler

--- src/engine/scheduler/SchedulerProject/Program.cs
+++ src/engine/scheduler/SchedulerProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using T = TaskManagement;
+﻿using TM = TaskManagement;
+using SL = SchedulerLib;
 
 namespace SchedulerProject
 {
@@ -6,30 +7,18 @@ namespace SchedulerProject
     {
         static void Main(string[] args)
         {
-            // Instantiate the TaskQueueManager
-            T.TaskQueueManager taskQueueManager = new T.TaskQueueManager();
-
-            // Create some tasks
-            T.Task task1 = new T.Task(1, "Task1");
-            T.Task task2 = new T.Task(2, "Task2");
-
-            // Enqueue tasks
+            TM.TaskQueueManager taskQueueManager = new TM.TaskQueueManager();
+            TM.Task task1 = new TM.Task(1, "Task1");
+            TM.Task task2 = new TM.Task(2, "Task2");
             taskQueueManager.EnqueueTask(task1);
             taskQueueManager.EnqueueTask(task2);
 
-            // Display the current queue size
-            Console.WriteLine($"Queue Size: {taskQueueManager.QueueSize()}");
+            SL.Scheduler scheduler = new SL.Scheduler(taskQueueManager);
+            scheduler.Start(TimeSpan.FromSeconds(2));
 
-            // Dequeue and display a task
-            T.Task nextTask = taskQueueManager.DequeueTask();
-
-            if (nextTask != null)
-            {
-                Console.WriteLine($"Dequeued Task: {nextTask.Name}, State: {nextTask.State}");
-            }
-
-            // Display the remaining queue size
-            Console.WriteLine($"Queue Size after Dequeue: {taskQueueManager.QueueSize()}");
+            // Keep the console app running
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
         }
     }
 }
